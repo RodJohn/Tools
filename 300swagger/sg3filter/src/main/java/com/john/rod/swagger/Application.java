4aa76780817a8ef43e5bd85fd0450f0c9be203cc
John <rod_john@163.com>
2018-04-18 17:01:01 +0800

    init

---
+++ 300swagger/sg3filter/src/main/java/com/john/rod/swagger/Application.java
@@ -0,0 +1,18 @@
+package com.john.rod.swagger;
+
+import com.spring4all.swagger.EnableSwagger2Doc;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+@EnableSwagger2Doc
+public class Application {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+
+}
